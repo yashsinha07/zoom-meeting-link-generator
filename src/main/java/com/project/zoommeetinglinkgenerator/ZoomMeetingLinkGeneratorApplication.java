cf77836996faa5f91ea7b11fd9412974544889f4
yashsinha <yashsinha7696@gmail.com>
2022-06-24 14:47:37 +0530

    initial commit.

---
+++ src/main/java/com/project/zoommeetinglinkgenerator/ZoomMeetingLinkGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.project.zoommeetinglinkgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomMeetingLinkGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoomMeetingLinkGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/zoommeetinglinkgenerator/ZoomMeetingLinkGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.zoommeetinglinkgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomMeetingLinkGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
